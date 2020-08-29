593903907cf199e2d3f2695991822f44e7001585
atl043 <andrewfourli@gmail.com>
2020-08-28 22:11:16 -0400

    make button redirect to projects

--- my-app/src/home-intro-section.js
+++ my-app/src/home-intro-section.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
 import MyNameIsTextComponet from "./baseComponents/MyNameIsTextComponet.jsx";
+import { Link } from 'react-router-dom';
+import { Button } from "react-bootstrap";
 class HomeIntro extends Component {
+
+    // onSubmit = () => {
+    //     console.log("test")
+    //     return <Link to='/projects' className='link'>Projects</Link>
+    // }
+
     render() {
         return (
             <section className="hero">
                 <div className="hero-content">
                     <MyNameIsTextComponet name={"Paul"} />
-
                     <h2 class="lead">
                         Nice to Meet You
                     </h2>
-
-                    <button type="button" class="hero-button">
-                        About Me
-                    </button>
+                    <Link to={'/projects'} className='link'>
+                        <Button variant="primary">
+                            Projects
+                        </Button>
+                    </Link>
                 </div>
             </section>
 
